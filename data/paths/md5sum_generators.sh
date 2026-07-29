eca73cbbc8cddc7a375ed09b4127338a64260321
Nicholas Brazeau <nbrazeau1@gmail.com>
2026-07-29 18:19:57 -0400

    update sheet for bad fastq

--- data/paths/md5sum_generators.sh
+++ data/paths/md5sum_generators.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
+mkdir -p nfb_md5sums
+
 ## Transcriptomics Data
 ### Nasal Lavage RNA-Seq (1.2 Tb)
-md5sum /Volumes/Private/CAGPM_Projects/2017_Prometheus/Data/RNA/Nasal/PRESAGE/Data/Burke_5229/BURKE_5229_181112B6_fastq/* > /Volumes/Private/CAGPM_Projects/2017_Prometheus/Workspace/nfb_md5sums/nasalrnaseq-BURKE_5229_181112B6_fastq_checksums.md5
+md5sum /nfs/datacommons/henao/Projects/2017_Prometheus/BURKE_5229_181112B6_fastq/* > nfb_md5sums/nasalrnaseq-BURKE_5229_181112B6_fastq_checksums.md5
 
 ### Whole Blood Peripheral Blood Mononuclear Cells (Batch 1: 293Gb; Batch 2: 467Gb)
-md5sum /Volumes/Private/CAGPM_Projects/2017_Prometheus/Data/RNA/Whole_Blood/Data/Batch1/fastq/* > /Volumes/Private/CAGPM_Projects/2017_Prometheus/Workspace/nfb_md5sums/bulkrnaseq-Batch1_checksums.md5
-md5sum /Volumes/Private/CAGPM_Projects/2017_Prometheus/Data/RNA/Whole_Blood/Data/Batch2/Janus_batch2_091218/batch2_fastq/* > /Volumes/Private/CAGPM_Projects/2017_Prometheus/Workspace/nfb_md5sums/bulkrnaseq-Batch2_checksums.md5
+md5sum /nfs/datacommons/henao/Projects/2017_Prometheus/whole_blood_Data_Batch1_fastq/* > nfb_md5sums/bulkrnaseq-Batch1_checksums.md5
+md5sum /nfs/datacommons/henao/Projects/2017_Prometheus/whole_blood_Data_Batch2_fastq/* > nfb_md5sums/bulkrnaseq-Batch2_checksums.md5
 
 ### Single Cell RNA Sequencing Data (pre-FASTQ: Batch 1 89 Gb; Batch 2 526 Gb; Gb)
-md5sum /Volumes/Private/CAGPM_Projects/2017_Prometheus/Data/RNA/Single_Cell/Data/Batch1/*  > /Volumes/Private/CAGPM_Projects/2017_Prometheus/Workspace/nfb_md5sums/scrnaseq-Batch1_checksums.md5
-md5sum /Volumes/Private/CAGPM_Projects/2017_Prometheus/Data/RNA/Single_Cell/Data/Batch2/*  > /Volumes/Private/CAGPM_Projects/2017_Prometheus/Workspace/nfb_md5sums/scrnaseq-Batch2_checksums.md5
-md5sum /Volumes/Private/CAGPM_Projects/2017_Prometheus/Data/RNA/Single_Cell/Data/Batch3/*  > /Volumes/Private/CAGPM_Projects/2017_Prometheus/Workspace/nfb_md5sums/scrnaseq-Batch3_checksums.md5
-
-
+cd /nfs/datacommons/henao/Projects/2017_Prometheus/single_cell_Data_Batch1; find . -type f ! -name "checksums.md5" -print0 | sort -z | xargs -0 md5sum > nfb_md5sums/scRNAseq-Batch1_checksums.md5
+cd /nfs/datacommons/henao/Projects/2017_Prometheus/single_cell_data_Batch2; find . -type f ! -name "checksums.md5" -print0 | sort -z | xargs -0 md5sum > nfb_md5sums/scRNAseq-Batch2_checksums.md5
+cd /nfs/datacommons/henao/Projects/2017_Prometheus/single_cell_data_Batch3; find . -type f ! -name "checksums.md5" -print0 | sort -z | xargs -0 md5sum > nfb_md5sums/scRNAseq-Batch3_checksums.md5
